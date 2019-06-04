755223e0710f0c1ddfe262a587ac55dfa07ddf78
Alexander Wu <alexander.wu7@gmail.com>
2019-06-04 15:57:21 -0700

    Bigram visualization

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ import time
 
 from flask import Flask, render_template, url_for, request
 from interpretation import Interpretation
+from classifier import SentimentClassifier, SarcasmClassifier
 
 
 app = Flask(__name__)
+sentiment_clf = SentimentClassifier()
+sarcasm_clf = SarcasmClassifier()
 
 @app.route('/')
 def index():
@@ -13,7 +16,10 @@ def index():
 @app.route('/interpret')
 def interpret():
     text = request.args.get('jsdata')
-    interpretation = Interpretation(text)
+    if True:
+        interpretation = Interpretation(sentiment_clf, text)
+    else:
+        interpretation = Interpretation(sarcasm_clf, text)
     return render_template('interpret.html', interpretation=interpretation)
 
 if __name__ == '__main__':
